c7add53d2359621119f0d6a5b23602f23ff1ce6b
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2023-01-17 23:00:41 -0500

    test

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -7,6 +7,7 @@ const Input = React.forwardRef((props, ref) => {
   const activate = () => {
     inputRef.current.focus();
   };
+
   useImperativeHandle(ref, () => {
     return {
       focus: activate,
